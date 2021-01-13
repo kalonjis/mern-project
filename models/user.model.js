10ba878e08c812239895e3bda7f2eed2c2cb77b7
Stephane <kalonjistephane@gmail.com>
2021-01-13 14:36:54 +0100

    cryptage password avec bcrypt

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const { isEmail } = require('validator')
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema(
     {
@@ -47,6 +48,13 @@ const userSchema = new mongoose.Schema(
     }
 
 )
+
+// play function before save into display: 'block'
+userSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+} )
 
 const UserModel = mongoose.model('user', userSchema);
 module.exports = UserModel;
