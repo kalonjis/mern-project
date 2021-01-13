6434fe4375248b9f2a8c9035a98f9dca6fc0413b
Stephane <kalonjistephane@gmail.com>
2021-01-13 13:40:50 +0100

    creation de auth.controllers.js

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const userRoutes = require('./routes/user.routes');
 require('dotenv').config({path: './config/.env'})
 require('./config/db')
 
 const app = express();
 
+//middleware
+
+//Routes
+app.use('/api/user', userRoutes);
+
+// server
 app.listen(process.env.PORT, ()=> {
     console.log(`listening on  port ${process.env.PORT}`)
 })
