05fcaa67b968f2d8682e63082b94222d7c4629b2
Stephane <kalonjistephane@gmail.com>
2021-01-17 21:53:43 +0100

    deletePost

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -49,6 +49,15 @@ module.exports.updatePost = (req, res) => {
 };
 
 module.exports.deletePost = (req, res) => {
-    
+    if (!ObjectID.isValid(req.params.id)) 
+    res.status(400).send('ID unknown : ' + req.params.id);
+
+    PostModel.findByIdAndRemove(
+      req.params.id,
+      (err, docs) => {
+          if(!err) res.send(docs);
+          else console.log('Delete error: '+ err);
+      }
+  ) 
 }
 
