5c975931457f344d17de9fd93c0a8d17be28c523
Stephane <kalonjistephane@gmail.com>
2021-01-18 12:17:14 +0100

    likePost & unlikePost

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -50,7 +50,7 @@ module.exports.updatePost = (req, res) => {
 
 module.exports.deletePost = (req, res) => {
     if (!ObjectID.isValid(req.params.id)) 
-    res.status(400).send('ID unknown : ' + req.params.id);
+        res.status(400).send('ID unknown : ' + req.params.id);
 
     PostModel.findByIdAndRemove(
       req.params.id,
@@ -61,3 +61,68 @@ module.exports.deletePost = (req, res) => {
   ) 
 }
 
+module.exports.likePost = async (req, res) => {
+    if (!ObjectID.isValid(req.params.id)) 
+        res.status(400).send('ID unknown : ' + req.params.id);
+    
+    try{
+        await PostModel.findByIdAndUpdate(
+            req.params.id,
+            {
+                $addToSet: { likers: req.body.id }
+            },
+            { new: true},
+            (err, docs)=>{
+                if (err) res.status(400).send(err);
+            }
+        );
+   
+        await UserModel.findByIdAndUpdate(
+            req.body.id,
+            {
+                $addToSet : { likes: req.params.id }
+            },
+            { new: true },
+            (err, docs) => {
+                if (err) res.status(400).send(err);
+                else res.status(201).json({message: "you like it! "});
+            }
+        );
+
+    } catch (err) {
+        return res.status(400).send(err);
+    }
+};
+
+module.exports.unlikePost = async (req, res) => {
+    if (!ObjectID.isValid(req.params.id)) 
+        res.status(400).send('ID unknown : ' + req.params.id);
+    
+    try{
+        await PostModel.findByIdAndUpdate(
+            req.params.id,
+            {
+                $pull: { likers: req.body.id }
+            },
+            { new: true, upsert: true},
+            (err, docs)=>{
+                if (err) res.status(400).send(err);
+            }
+        );
+    
+        await UserModel.findByIdAndUpdate(
+            req.body.id,
+            {
+                $pull : { likes: req.params.id }
+            },
+            { new: true, upsert: true },
+            (err, docs) => {
+                if (err) res.status(400).send(err);
+                else res.status(201).json({message: "you don't like it anymore! "});
+            }
+        );
+
+    } catch (err) {
+        return res.status(400).send(err);
+    }
+}
