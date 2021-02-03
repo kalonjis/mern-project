2d05ad34adbd55e8de7583d5f48e462646f5e408
Stephane <kalonjistephane@gmail.com>
2021-02-03 12:53:24 +0100

    Navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,43 @@
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+import { UidContext } from './AppContext';
+
+const Navbar = ()=>{
+    const uid = useContext(UidContext)
+
+    return (
+        <nav>
+            <div className ='nav-container'> 
+                <div className='logo'>
+                    <NavLink exact to='/'>
+                        <div className='logo'>
+                            <img src='../img/icon.png' alt='icon of ratoon' />
+                            <h3>Steby Network</h3>
+                        </div>
+                    </NavLink>
+                </div>
+                {uid? (
+                    <ul>
+                        <li></li>
+                        <li className="welcome">
+                            <NavLink exact to='/profil'>
+                                <h5>Bienvenu 'valeur dynamique'</h5>
+                            </NavLink>
+                        </li>
+                        Logo logout
+                    </ul>
+                ) : (
+                    <ul>
+                        <li></li>
+                        <li>
+                            <NavLink exact to='/profil'>
+                                <img src="../img/icons/login.svg" alt='login'/>
+                            </NavLink>
+                        </li>
+                    </ul>
+                )}
+            </div>
+        </nav>
+    )
+}
+export default Navbar
