3b5be392cf7563c3e299983dd1c5afdc760557c4
Stephane <kalonjistephane@gmail.com>
2021-02-16 17:04:15 +0100

    NewPostForm - preview

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UidContext } from '../components/AppContext';
 import LeftNav from '../components/LeftNav'
+import NewPostForm from '../components/Post/NewPostForm';
+import Log from '../components/log/Log'
 import Thread from '../components/Thread';
-// import Log from '../components/log/Log'
 
 const Home = () =>{
+    const uid = useContext(UidContext);
+
     return (
         <div className="home">
             <LeftNav />
             <div className="main">
+                <div className="home-header">
+                    {uid? <NewPostForm/>: <Log signin={true} signup={false} />}
+                </div>
             <Thread/>
             </div>
         </div> 
